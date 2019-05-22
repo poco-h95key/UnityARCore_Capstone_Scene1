58c7dfd63d5853c313707a1c07cdf300fee153b7
minkihong <palear39@naver.com>
2019-05-22 15:17:00 +0900

    Initial commit

---
+++ Assets/scripts/Editor/Bundle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Bundle : Editor
+{
+    [MenuItem("Assets/ Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles(@"/Users/hongmingi//Desktop/AssetBundle", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+
+    }
+}
